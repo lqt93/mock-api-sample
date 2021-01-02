aa9c8a6fea1a72591f0aa10085848f98cb10a4ee
Thinh Le <lqthinh93@gmail.com>
2021-01-02 11:30:04 +0700

    add css to post page

--- domain/Blog/PostPage/index.jsx
+++ domain/Blog/PostPage/index.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import BlogTypes from 'types/blog';
 
 const PostPage = ({ post }) => (
-  <div>
-    <div>
-      {post.id}
-      {' - '}
-      {post.title}
-    </div>
-    <div>
-      {post.content}
+  <div className="flex justify-center my-8">
+    <div className="container font-mono">
+      <Link href="/blog/">
+        <a>Back</a>
+      </Link>
+      <div className="text-8xl text-center">{post.title}</div>
+      <div className="rounded-md my-5 flex justify-center">
+        <img
+          src={post.image}
+          alt={post.title}
+          className="object-cover rounded-md"
+        />
+      </div>
+      <div className="w-full p-1.5">
+        <div className="text-lg">{post.content}</div>
+      </div>
     </div>
   </div>
 );
