376e344b2af8039714e228e80aac165003dc77fb
Thinh Le <lqthinh93@gmail.com>
2021-01-05 11:39:29 +0700

    beautify home page

--- domain/Home/index.jsx
+++ domain/Home/index.jsx
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
 const Home = () => (
-  <div>
-    <h1> Welcome Home </h1>
-    <div>
+  <div className="w-full h-screen flex justify-center items-center flex-col bg-gray-50 font-mono">
+    <h1 className="text-3xl my-5"> Welcome Home </h1>
+    <div className="text-blue-700">
       <Link href="/blog">
         <a> Go to blog </a>
       </Link>
